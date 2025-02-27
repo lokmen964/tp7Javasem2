a1b97cbbfba163dbb14546b7be14028dc4a20373
lokmen964 <lokmenezza2@gmail.com>
2025-02-27 10:56:27 +0100

    app1

---
+++ tp7/src/app2.java
@@ -0,0 +1,30 @@
+
+public class app2 {
+        
+    public app2() {
+    }
+    public static void main(String[] args) {
+        int[][] M= new int [30][30];
+        int nl,nc;
+        do{
+        	System.out.println("donner les nombres de lignes");
+        	nl=Clavier.lireInt();
+        	System.out.println ("donner les nombres de colonnes");
+        	nc=Clavier.lireInt();
+        }while(nl>30 || nc<30);
+        for (i=0;i<nc;i++){
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }  
+}
---
+++ tp7/src/tp7.java
@@ -0,0 +1,17 @@
+/**
+ * @(#)tp7.java
+ *
+ * tp7 application
+ *
+ * @author 
+ * @version 1.00 2025/2/20
+ */
+ 
+public class tp7 {
+    
+    public static void main(String[] args) {
+    	
+    	// TODO, add your application code
+    	System.out.println("Hello World!");
+    }
+}
